4c2581fd634dd53eab61ebc665ee2c548e3ef5e5
hupe13 <hupe13@t-online.de>
2021-04-18 20:30:04 +0200

    settings zoomhome

--- admin.php
+++ admin.php
@@ -1,6 +1,8 @@
 <?php
 // Admin Menu
 
+include "admin/generic-options.php";
+
 add_action('admin_init', 'testleafext_init' );
 add_action('admin_menu', 'testleafext_add_page', 99);
 
@@ -23,13 +25,22 @@ function testleafext_do_page() {
 	?>
 	<div class="wrap">
 	<h2>Extensions Tests Options</h2>
+<?php
+	$leafext_plugin_name = basename(dirname(  __FILE__  ));
+	echo '<div class="wrap">
+	<h2>Extensions for Leaflet Map Options</h2>';
 
-	Nothing
-
-
+	echo '<form method="post" action="options.php">';
+			settings_fields('leafext_settings_cluster');
+			do_settings_sections( 'leafext_settings_cluster' );
+			
+	echo '<p class="submit">';
+	echo '<input type="submit" class="button-primary" value="';
+	_e('Save Changes');
+	echo '" />';
+	echo '</p>';
+	echo '</form>';
+?>
 	</div>
 	<?php
 }
-
-// Sanitize and validate input. Accepts an array, return a sanitized array.
-
