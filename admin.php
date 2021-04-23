e71fe96478f18529d71d8922d7f3683d5dead181
hupe13 <hupe13@phw-web.de>
2021-04-23 17:29:34 +0200

    Help

--- admin.php
+++ admin.php
@@ -2,6 +2,7 @@
 // Admin Menu
 
 include "admin/markercluster.php";
+include "admin/help.php";
 
 add_action('admin_init', 'testleafext_init' );
 add_action('admin_menu', 'testleafext_add_page', 99);
@@ -22,19 +23,38 @@ function testleafext_add_page() {
 // Draw the menu page itself
 function testleafext_do_page() {
 	//var_dump($options);
+	$leafext_plugin_name = basename(dirname(  __FILE__  ));
 	?>
 	<div class="wrap">
 	<h2>Extensions Tests Options</h2>
 <?php
-	$leafext_plugin_name = basename(dirname(  __FILE__  ));
+	$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'help';
+
+	echo '<h3 class="nav-tab-wrapper">';
+
+	echo '<a href="?page='.$leafext_plugin_name.'&tab=cluster" class="nav-tab';
+	echo $active_tab == 'cluster' ? ' nav-tab-active' : '';
+	echo '">Markercluster</a>';
+	echo '<a href="?page='.$leafext_plugin_name.'&tab=help" class="nav-tab';
+	echo $active_tab == 'help' ? ' nav-tab-active' : '';
+	echo '">Hilfe!</a>';
+
+	echo '</h3>';
+
 	echo '<div class="wrap">
 	<h2>Extensions for Leaflet Map Options</h2>';
+	if( $active_tab != 'help' ) {
 	echo '<form method="post" action="options.php">';
+	if( $active_tab == 'cluster' ) {
 			settings_fields('leafext_settings_cluster');
 			do_settings_sections( 'leafext_settings_cluster' );
-			submit_button();
-			
+	}
+	submit_button();
 	echo '</form>';
+}
+	if( $active_tab == 'help' ) {
+		echo leafext_help_text();
+	}
 ?>
 	</div>
 	<?php
---
+++ admin/help.php
@@ -0,0 +1,24 @@
+<?php
+function leafext_help_text () {
+    $text =
+	"<p>".
+    '<h2>Multiple Maps with elevation profile on one page</h2>
+    <pre>
+<code>[leaflet-map]
+[testelevation gpx="url_to_file1" summary=0]
+[leaflet-map]
+[testelevation gpx="url_to_file2" summary=1]</code>
+</pre>
+    <h2>Geojson Popup on mouseover</h2>
+    <p>Use it like <code>[hover]</code></p>
+<pre>
+<code>[testhover]</code>
+</pre>
+    <h2>Markercluster</h2>
+    <p>You can define radius (maxClusterRadius) and zoom (disableClusteringAtZoom) in Settings -&gt; Leaflet Map -&gt; Extensions Tests or per map.</p>
+<pre>
+<code>[testcluster radius="..." zoom="..."]</code>
+</pre>'
+."</p>";
+	return $text;
+}
