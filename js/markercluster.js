c16e31f1ba6d52bd1a78e18ddf5254adf082dbd9
hupe13 <hupe13@users.noreply.github.com>
2021-04-29 15:43:05 +0200

    markercluster

--- js/markercluster.js
+++ js/markercluster.js
@@ -1,22 +1,26 @@
 // For use with only one map on a webpage
-console.log("cluster.maxZoom");
-console.log(cluster.zoom);
-console.log("cluster.radius");
-console.log(cluster.radius);
+console.log(cluster);
+console.log("cluster.Zoom"+cluster.zoom);
+console.log("cluster.radius"+cluster.radius);
+console.log("cluster.spiderfy"+cluster.spiderfy);
 
 window.WPLeafletMapPlugin = window.WPLeafletMapPlugin || [];
 window.WPLeafletMapPlugin.push(function () {
 	var map = window.WPLeafletMapPlugin.getCurrentMap();
 	if ( WPLeafletMapPlugin.markers.length > 0 ) {
 		map.options.maxZoom = 19;
+		var myzoom = cluster.zoom;
+		if ( cluster.zoom == "0" ) myzoom = false;
+		console.log("myzoom"+myzoom);
 		clmarkers = L.markerClusterGroup({
 			maxClusterRadius: function(radius)
 			//	{ return 60; },
 			//	{return ((radius <= 13) ? 50 : 30);},
 				{ return cluster.radius; },
-			spiderfyOnMaxZoom: true,
+			spiderfyOnMaxZoom: cluster.spiderfy,
 			// ab welcher Zoomstufe es nicht mehr tiefer geht, dann wird gespidert.
-			disableClusteringAtZoom: cluster.zoom,
+			//disableClusteringAtZoom: cluster.zoom,
+			disableClusteringAtZoom: myzoom,
 		});
 		for (var i = 0; i < WPLeafletMapPlugin.markers.length; i++) {
 			var a = WPLeafletMapPlugin.markers[i];
