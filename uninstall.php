53d24184d661c7bffa9d86b356ff9520708efc3e
hupe13 <hupe13@users.noreply.github.com>
2021-05-03 13:30:06 +0200

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+global $wpdb;
+$option_names = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'testleafext_%' " );
+foreach ( $option_names as $key=>$value ) {
+  delete_option($value->option_name);
+  // for site options in Multisite
+  delete_site_option($value->option_name);
+}
+?>
